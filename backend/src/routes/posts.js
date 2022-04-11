d14b924159e6a33dd5cea15f7189286a5e8525ab
moathabuhamad <moathabuhamad@outlook.com>
2022-04-11 13:37:55 +0300

    error handling

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { user } = require('pg/lib/defaults');
 const { where } = require('sequelize/types');
-const Posts = require('../model/posts')
+const database = require('../db/models/index')
+
 const router = express.Router()
 
 // Posts Route
@@ -10,45 +12,54 @@ router.get("/Posts/:id", getSinglePostsHandler);
 router.put("/Posts/:id", updatePostInfoHandler);
 router.delete("/Posts/:id", deletePostHandler);
 router.get("/Posts/:author", getPostsFromUserHandler);
+router.get("/Posts/:community_id", getPostsFromCommunityHandler);
+router.get("/Posts/", getUserPostsFromCommunity);
 
 // Controllers
 
 //Get All Posts
 async function getPostsHandler(req, res) {
-  let fetchedPost = await Posts.findAll();
+  let fetchedPost = await database.posts.findAll();
   res.status(200).json(fetchedPost);
 }
 //Get single Posts
 async function getSinglePostsHandler(req, res) {
   let pid = parseInt(req.params.id);
-  let fetchedPost = await Posts.findOne({ where: { id: pid } });
+  let fetchedPost = await database.posts.findOne({ where: { id: pid } });
   res.json(fetchedPost);
 }
 //Update single Posts
 async function updatePostInfoHandler(req, res) {
   let pid = req.params.id;
-  let toUpdate = await Posts.findOne({ where: { id: pid } });
+  let toUpdate = await database.posts.findOne({ where: { id: pid } });
   let updatedPost = await toUpdate.update(req.body);
   res.status(201).json(updatedPost);
 }
 //Delete single Posts
 async function deletePostHandler(req, res) {
   let pid = parseInt(req.params.id);
-  let fetchedPost = await Posts.destroy({ where: { id: pid } });
+  let fetchedPost = await database.posts.destroy({ where: { id: pid } });
   res.status(201).json(fetchedPost);
 }
 
 
-//Get All Posts from a single user
+//Get All Posts from a single user from all communities
 async function getPostsFromUserHandler(req, res) {
   let uid = parseInt(req.params.author)
   let fetchedPost = await Post.findAll({ where: { author: uid }});
   res.status(200).json(fetchedPost);
 }
 
+//Get All Posts from a single user from a single community
+async function getUserPostsFromCommunity(req, res) {
+  let uid = parseInt(req.body.author);
+  let cid = parseInt(req.body.community_id);
+  let fetchedPost = await user.findAll({ where: { author: uid , community_id:cid } , include:[database.posts] });
+  res.status(200).json(fetchedPost);
+}
 
 //Get All Posts from a single community
-async function getPostsFromUserHandler(req, res) {
+async function getPostsFromCommunityHandler(req, res) {
   let cid = parseInt(req.params.community_id)
   let fetchedPost = await Post.findAll({ where: { community_id: cid }});
   res.status(200).json(fetchedPost);
