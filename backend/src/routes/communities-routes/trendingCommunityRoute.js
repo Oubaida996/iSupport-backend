5065a1947ac45321923b67fc3f7b8431240ff372
karamalqinneh <karamalqinneh@live.com>
2022-04-17 14:13:34 +0300

    updated the trending community route

--- backend/src/routes/communities-routes/trendingCommunityRoute.js
+++ backend/src/routes/communities-routes/trendingCommunityRoute.js
@@ -6,14 +6,20 @@ const router = express.Router();
 router.get("/trending", trendingCommunity);
 
 async function trendingCommunity(req, res) {
-  let results = await database.posts.findAll({
+  let postsRaw = await database.posts.findAll({
     attributes: ["community_id"],
     group: "community_id",
     order: [database.sequelize.fn("COUNT", database.sequelize.col("posts"))],
     limit: 5,
   });
-  console.log(results);
-  res.status(201).json(results);
+  let trendingCommunitiesID = postsRaw.map((ele) => ele["community_id"]);
+  let communities = await database.communities.findAll();
+  let returnedCommunitiesNames = communities
+    .filter((ele) =>
+      trendingCommunitiesID.indexOf(ele.dataValues.community_name)
+    )
+    .map((ele) => ele["community_name"]);
+  res.status(201).json(returnedCommunitiesNames);
 }
 
 module.exports = router;
