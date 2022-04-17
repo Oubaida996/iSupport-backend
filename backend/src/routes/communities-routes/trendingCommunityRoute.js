cec96ca9e868ca3e8164d1ec555d31b738a483be
karamalqinneh <karamalqinneh@live.com>
2022-04-17 14:03:26 +0300

    fixed assigned routes bugs

--- backend/src/routes/communities-routes/trendingCommunityRoute.js
+++ backend/src/routes/communities-routes/trendingCommunityRoute.js
@@ -2,28 +2,18 @@
 const express = require("express");
 const database = require("../../db/models/index");
 const router = express.Router();
-const { QueryTypes } = require("sequelize");
 
-router.get("/trending/:id", getTrending);
-router.get("/trending", trendingCommity);
+router.get("/trending", trendingCommunity);
 
-async function trendingCommity(req, res) {
-  const [results, metadata] = await database.sequelize.query(
-    `SELECT  count(posts)
-        FROM posts
-        GROUP BY community_id
-        ORDER BY COUNT(posts) DESC
-        LIMIT 5;`
-  );
+async function trendingCommunity(req, res) {
+  let results = await database.posts.findAll({
+    attributes: ["community_id"],
+    group: "community_id",
+    order: [database.sequelize.fn("COUNT", database.sequelize.col("posts"))],
+    limit: 5,
+  });
+  console.log(results);
   res.status(201).json(results);
-  // res.send(req.body);
-}
-
-async function getTrending(req, res) {
-  let id = req.params.id;
-  let allData = await database.communities.findOne({ where: { id: id } });
-  res.status(200).send(allData);
-  // res.send('test trending')
 }
 
 module.exports = router;
