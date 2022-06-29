8d20d802a207ed1063c55308a8a7a78103a1e01a
Obieda Akilan <obieda.jehad@gmail.com>
2022-06-29 10:48:05 +0300

    ssss

--- src/routes/community/leaderborad.js
+++ src/routes/community/leaderborad.js
@@ -7,26 +7,33 @@ router.get("/community/:id/leaderboard", getCommunityLeaderboard);
 
 
 async function getCommunityLeaderboard(req, res) {
-  let id = parseInt(req.params.id);
-  const result = await database.posts.findAll({
-    attributes: [
-      "author",
-      [database.sequelize.fn("COUNT", database.sequelize.col("author")), 'progress'],
-    ],
-    group: ["author", "user.id"],
-    order: [database.sequelize.fn("COUNT", database.sequelize.col("author"))],
-    limit: 3,
-    include: [database.users],
-    where: { community_id: id },
-  });
+  try {
+    let id = parseInt(req.params.id);
+    const result = await database.posts.findAll({
+      attributes: [
+        "author",
+        [database.sequelize.fn("COUNT", database.sequelize.col("author")), 'progress'],
+      ],
+      group: ["author", "user.id"],
+      order: [database.sequelize.fn("COUNT", database.sequelize.col("author"))],
+      limit: 3,
+      include: [database.users],
+      where: { community_id: id },
+    });
+    let response = result.map(ele => {
+      return {
+        username: ele.user.username,
+        progress: ele.dataValues.progress,
+      }
+    }).sort((a, b) => b.progress - a.progress);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 
-  let response = result.map(ele => {
-    return {
-      username: ele.user.username,
-      progress: ele.dataValues.progress,
-    }
-  }).sort((a, b) => b.progress - a.progress);
-  res.status(200).json(response);
+
+
+
 }
 
 module.exports = router;
--- src/routes/community/personalProgress.js
+++ src/routes/community/personalProgress.js
@@ -9,22 +9,27 @@ router.get("/community/:id/personalProgress", bearerAuth, getpersonalProgress);
 
 
 async function getpersonalProgress(req, res) {
-  let id = parseInt(req.params.id);
-  let userid = req.user.dataValues.id;
-  const personalProgress = await database.posts.findAll({
-    where: { community_id: id, author: userid },
-  });
-  let tasksHistory = personalProgress.map((ele) => {
-    let output = {
-      post_id: ele.dataValues.id,
-      post_title: ele.dataValues.post_title,
-      post_body: ele.dataValues.post_body,
-    };
-    return output;
-  });
-  res
-    .status(200)
-    .json({ numberOfTasks: personalProgress.length, history: tasksHistory });
+  try {
+    let id = parseInt(req.params.id);
+    let userid = req.user.dataValues.id;
+    const personalProgress = await database.posts.findAll({
+      where: { community_id: id, author: userid },
+    });
+    let tasksHistory = personalProgress.map((ele) => {
+      let output = {
+        post_id: ele.dataValues.id,
+        post_title: ele.dataValues.post_title,
+        post_body: ele.dataValues.post_body,
+      };
+      return output;
+    });
+    res
+      .status(200)
+      .json({ numberOfTasks: personalProgress.length, history: tasksHistory });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
 }
 
 module.exports = router;
